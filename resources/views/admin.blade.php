ba6d1bf9ddd2a48509baab6012426f5346139be6
Thur-melo <thurvictor2006@gmail.com>
2024-09-24 17:36:47 -0300

    Adm atualizada by Vini

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,66 +1,91 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
     <link rel="stylesheet" href="{{url('assets/css/admin.css')}}">
-    <link rel="stylesheet" href="{{url('assets/css/navbarAdm.css')}}">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Admin</title>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
+
 <body>
-
-<nav class="navbar">
-    <h1 class="title">Eae</h1>
-    <div class="user">
-    <h6>eu</h6>
-    </div>
-</nav>
-    <div class="main">
-        <div class="sidebarAdm">
-
-        <a href="{{ route('adminHome') }}" class="sidebarBotao ">
-            <span class="material-symbols-outlined">search</span>
-            <h2>Perguntas</h2>
-        </a>
-        <a href="{{ route('admin') }}"class="sidebarBotao active">
-            <span class="material-symbols-outlined">search</span>
-            <h2>Análises</h2>
-        </a>
-
+    <div class="menu-lateral">
+        <div class="brand-name">
+            <h1>Conectec</h1>
         </div>
-        <div class="ConteudoAdm">
-            <div class="cardAdm" id="CardQntUser">
-                    <span class="material-symbols-outlined" style=" font-size: 32px; /* Ajuste o tamanho do ícone aqui */
-                    color: white;">
-                    check_circle
-                    </span>
-                <h3> {{ $qnt_users }}</h3>
-                <h3>Total Usuários</h3>
-            </div>
-            <div class="cardAdm" id="CardQntUser2" >
-                    <span class="material-symbols-outlined" style=" font-size: 32px; /* Ajuste o tamanho do ícone aqui */
-                    color: white;">
-                    check_circle
-                    </span>
-                <h3> {{ $qnt_aprovados }}</h3>
-                <h3>Perguntas Aprovadas</h3>
-            </div>
-            <div class="cardAdm" id="CardQntUser3">
-                    <span class="material-symbols-outlined" style=" font-size: 32px; /* Ajuste o tamanho do ícone aqui */
-                    color: white;">
-                    check_circle
-                    </span>
-                <h3> {{ $qnt_pendentes }}</h3>
-                <h3>Perguntas Pendentes:</h3>
+        <ul>
+            <li> <i class="fa-solid fa-chart-line" id="icons"> </i>Dashboard</li>
+            <li> <i class="fa-solid fa-users" id="icons"> </i>Usuários</li>
+            <li> <i class="fa-solid fa-user" id="icons"> </i>Administrador</li>
+            <li> <i class="fa-regular fa-comment" id="icons"></i>Chat</li>
+        </ul>
+        
+        <button class="logout-btn"><i class="fa-solid fa-arrow-right-from-bracket" id="icons"></i>Logout</button>
+    </div>
+
+    <div class="container">
+        <div class="header">
+            <div class="nav">
+                <div class="text">
+                </div>
+                <div class="buscar">
+                    <input type="text" placeholder="Pesquisar...">
+                </div>
+                <div class="usuario">
+                    <img src="{{url('assets/img/perfil.jpg')}}" alt="Perfil">
+                </div>
             </div>
         </div>
-    </div>
- 
+        <div class="content">
+            <div class="cards">
+                <div class="card">
+                    <div class="box">
+                        <h2>sim</h2>
+                        <h3>Usuários</h3>
+                    </div>
+                    <div class="icon-case">
+                        <i class="fa-solid fa-user" id="icons-card"></i>
+                    </div>
+                </div>
 
+                <div class="card">
+                    <div class="box">
+                        <h1>1m</h1>
+                        <h3>Usuários</h3>
+                    </div>
+                    <div class="icon-case">
+                        <i class="fa-solid fa-user" id="icons-card"></i>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="box">
+                        <h1>1m</h1>
+                        <h3>Usuários</h3>
+                    </div>
+                    <div class="icon-case">
+                        <i class="fa-solid fa-user" id="icons-card"></i>
+                    </div>
+                </div>
+
+                
+
+                <div class="card">
+                    <div class="box">
+
+                        <h3>Usuários</h3>
+                    </div>
+                    <div class="icon-case">
+                        <i class="fa-solid fa-user" id="icons-card"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="content2">
+                <div class="tabela-usuarios"></div>
+                <div class="novo-usuarios"></div>
+            </div>
+        </div>
 </body>
+
 </html>
