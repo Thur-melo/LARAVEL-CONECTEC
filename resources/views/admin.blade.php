ac5c34b9e2b3b49afe9c3a1cb159733a5e06da37
Thur-melo <thurvictor2006@gmail.com>
2024-09-25 17:30:44 -0300

    Adm atualizada by Andre e vini

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,21 +7,24 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{url('assets/css/admin.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 
 <body>
     <div class="menu-lateral">
         <div class="brand-name">
-            <h1>Conectec</h1>
+            <img src="{{url('assets/img/logoConectec.png')}}" id="logo" alt="">
         </div>
         <ul>
-            <li> <i class="fa-solid fa-chart-line" id="icons"> </i>Dashboard</li>
-            <li> <i class="fa-solid fa-users" id="icons"> </i>Usuários</li>
-            <li> <i class="fa-solid fa-user" id="icons"> </i>Administrador</li>
-            <li> <i class="fa-regular fa-comment" id="icons"></i>Chat</li>
+            <li> <span class="material-icons" id="icons">post_add</span> Postagens </li>
+            <li> <span class="material-icons" id="icons">people</span> Usuários </li>
+            <li> <span class="material-icons" id="icons">person</span> Administrador </li>
+            <li> <span class="material-icons" id="icons">chat</span> Chat </li>
+
+            <li id="logout"> <span class="material-icons" id="icons">logout</span> Sair </li>
+
         </ul>
-        
-        <button class="logout-btn"><i class="fa-solid fa-arrow-right-from-bracket" id="icons"></i>Logout</button>
     </div>
 
     <div class="container">
@@ -39,45 +42,46 @@
         </div>
         <div class="content">
             <div class="cards">
-                <div class="card">
-                    <div class="box">
-                        <h2>sim</h2>
-                        <h3>Usuários</h3>
-                    </div>
-                    <div class="icon-case">
-                        <i class="fa-solid fa-user" id="icons-card"></i>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div class="box">
-                        <h1>1m</h1>
-                        <h3>Usuários</h3>
-                    </div>
-                    <div class="icon-case">
-                        <i class="fa-solid fa-user" id="icons-card"></i>
-                    </div>
-                </div>
-
-                <div class="card">
-                    <div class="box">
-                        <h1>1m</h1>
-                        <h3>Usuários</h3>
-                    </div>
-                    <div class="icon-case">
-                        <i class="fa-solid fa-user" id="icons-card"></i>
-                    </div>
-                </div>
-
                 
+                                <div class="card">
+                                    <div class="box">
+                                        <h1>{{$qnt_aprovados}}</h1>
+                                        <h3>Respostas aprovadas</h3>
+                                    </div>
+                                    <div class="icon-case">
+                                        <span class="material-icons" id="icons-card">thumb_up</span>
+                                    </div>
+                                </div>
+                
+                                <div class="card">
+                                    <div class="box">
+                                        <h1>{{$qnt_pendentes}}</h1>
+                                        <h3>Perguntas pendentes</h3>
+                                    </div>
+                                    <div class="icon-case">
+                                        <span class="material-icons" id="icons-card">pending_actions</span>
+                                    </div>
+                                </div>
+
+                <div class="card">
+                    <div class="box">
+                        <h2>{{$qnt_users}}</h2>
+                        <h3>Posts em Análise</h3>
+                    </div>
+                    <div class="icon-case">
+                        <span class="material-icons" id="icons-card">manage_search</span>
+                    </div>
+                </div>
+
+
 
                 <div class="card">
                     <div class="box">
 
-                        <h3>Usuários</h3>
+                        <h3>Posts Banidos</h3>
                     </div>
                     <div class="icon-case">
-                        <i class="fa-solid fa-user" id="icons-card"></i>
+                    <span class="material-icons" id="icons-card">block</span>
                     </div>
                 </div>
             </div>
