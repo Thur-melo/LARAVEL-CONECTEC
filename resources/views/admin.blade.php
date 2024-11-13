184a45997973f450bb84fc95451538b4e1e71503
crisegreg171 <crieegreg171@gmai.com>
2024-11-13 03:38:03 -0300

    sim3

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -21,17 +21,18 @@
     <div class="sidebar">
         <img src="{{url('assets/img/logoConectec4.png')}}" class="logo-sidebar" alt="">
         <ul>
-            <li><a href="#">Inicio</a></li>
-            <li><a href="#">Úsuario</a></li>
-            <li><a href="#">Postagens</a></li>
-            <li><a href="#">Postagens</a></li>
-            <li class="logout">
+            <li><a href= "{{ route('admin') }}" >Úsuario</a></li>
+            <li><a href= "{{ route('adminHome') }}" >Postagens</a></li>
+            <li><a href="{{ route('preferenciasLista') }}">Preferências</a></li>
+               
+                <li class="logout">
     <a href="#logout">Logout <span class="material-symbols-outlined icon-logout">logout</span></a>
 
 </li>
 
 </li>
         </ul>
+    </div>
     </div>
     <!-- sidebar fim -->
      <div class="container">
